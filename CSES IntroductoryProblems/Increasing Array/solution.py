4d80eb156f621ebd3595089a97abb731c802ac4a
Buddhi Wijenayake <wijenayakebuddhi34802@gmail.com>
2023-04-07 12:52:13 +0530

    Create solution.py

---
+++ CSES IntroductoryProblems/Increasing Array/solution.py
@@ -0,0 +1,12 @@
+n=int(input())
+
+arr=list(map(int,input().split()))
+
+steps=0
+
+for i in range(1,len(arr)):
+    if arr[i-1]>arr[i]:
+        steps+=arr[i-1]-arr[i]
+        arr[i]=arr[i-1]
+
+print(steps)
