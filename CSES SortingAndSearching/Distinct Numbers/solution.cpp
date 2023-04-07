80e294ce79e63922f0311bc93094c73eaed63483
Buddhi Wijenayake <wijenayakebuddhi34802@gmail.com>
2023-04-08 00:21:43 +0530

    Updated Solution.cpp

--- CSES SortingAndSearching/Distinct Numbers/solution.cpp
+++ CSES SortingAndSearching/Distinct Numbers/solution.cpp
@@ -5,15 +5,15 @@ int main()
 {
 	long int n;
 	scanf("%ld",&n);
-	long int A[n];
+	long *A = (long*) calloc(n,sizeof(long));
 	
-	for (long int i=0;i<n;i++){
+	for (int i=0;i<n;i++){
 		scanf("%ld",&A[i]);
 	}
 	
 	std::unordered_set<long int> s(A, A + n);
 
 	std::cout << s.size();
-
+    free(A);
 	return 0;
 }
