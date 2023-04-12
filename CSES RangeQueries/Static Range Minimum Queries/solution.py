4dff08266ff49eff319416175c4546b1015fa499
Buddhi Wijenayake <wijenayakebuddhi34802@gmail.com>
2023-04-12 20:06:27 +0530

    solution.py

--- CSES RangeQueries/Static Range Minimum Queries/solution.py
+++ CSES RangeQueries/Static Range Minimum Queries/solution.py
@@ -1,6 +1,11 @@
 n,q=map(int,input().split())
 
 arr=list(map(int,input().split()))
+############# SPARSE TABLE #########################
+
+
+
+
 
 for _ in range(q):
     a,b=map(int,input().split())
